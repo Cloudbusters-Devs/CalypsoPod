248c6503a8422a38f046f0a5a7f92111c89de3a2
geova <geova>
2020-03-31 16:15:35 +0200

    Initial Commit

---
+++ Calypso/Calypso.h
@@ -0,0 +1,19 @@
+//
+//  Calypso.h
+//  Calypso
+//
+//  Created by Antonino Rossi on 31/03/2020.
+//  Copyright © 2020 Cloudbusters.inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Calypso.
+FOUNDATION_EXPORT double CalypsoVersionNumber;
+
+//! Project version string for Calypso.
+FOUNDATION_EXPORT const unsigned char CalypsoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Calypso/PublicHeader.h>
+
+
